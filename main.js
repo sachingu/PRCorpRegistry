94ef014bdfcc664de79a111133e7a7916442f12f
Dilpreet Kaur <dilpreetmarwaha@gmail.com>
2019-07-17 15:24:11 +0530

    changes for the counter on number of requests

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const path = require("path");
 const linkScraper = require("./links");
 const corporationScraper = require("./detail");
 const storageService = require("./storage");
+var browser,page;
 
 const searchUrl = 'https://prcorpfiling.f1hst.com/CorporationSearch.aspx?rl=true';
 async function main() {
@@ -11,13 +12,7 @@ async function main() {
   const rangeStart = !isNaN(args[0]) ? parseInt(args[0]) : 1;
   const rangeEnd = !isNaN(args[1]) ? parseInt(args[1]) : rangeStart;
   let registerNumber;
-  const browser = await puppeteer.launch({
-    headless: true
-  });
-  const page = await browser.newPage();
-  await page.setExtraHTTPHeaders({
-    'accept-language': 'en-US,en;q=0.8'
-  });
+  await initializeBrowser();
 
   const outputDirectory = './output'
   if (!fs.existsSync(outputDirectory)){
@@ -25,6 +20,7 @@ async function main() {
   }
 
   let registryData = [];
+  let counter=0;
   for (registerNumber = rangeStart; registerNumber <= rangeEnd; registerNumber++) {
     try {
       await page.goto(searchUrl);
@@ -39,8 +35,11 @@ async function main() {
     
           for (let corporation of registryEntry.corporations) {
             const corporationDetail = await corporationScraper.getCorporationData(corporation.url, page);
-            //see if corporation exists in db
-            storageService.checkForRecord(registerNumber,corporationDetail.generalDetails.crpName.replace("'",""));
+            if(++counter==10)
+            {
+              await browser.close();
+              await initializeBrowser();
+            }
             Object.assign(corporation, corporationDetail);
           }
         }
@@ -64,5 +63,14 @@ async function main() {
   outputPath = path.join(outputDirectory, `${new Date().getTime()}.json`);
   fs.writeFileSync(outputPath, JSON.stringify(registryData));
 }
-
+async function initializeBrowser()
+{
+  browser = await puppeteer.launch({
+    headless: true
+  });
+  page = await browser.newPage();
+  await page.setExtraHTTPHeaders({
+    'accept-language': 'en-US,en;q=0.8'
+  });
+}
 main();
